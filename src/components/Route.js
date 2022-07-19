b1e18dc4238f14ac986839619b63c9a6ce7a5258
Dawid Szarwark <daq131@gmail.com>
2022-07-19 23:57:06 +0200

    build a navigation

--- src/components/Route.js
+++ src/components/Route.js
@@ -1,4 +1,14 @@
+import { useEffect, useState } from "react";
+
 const Route = ({ path, children }) => {
+  useEffect(() => {
+    const onLocationChange = () => {
+      console.log(onLocationChange);
+    }
+
+    window.addEventListener('popstate', onLocationChange)
+  }, []);
+
   return window.location.pathname === path ? children : null;
 };
 
